38a7af63307b0b323a0a8cbdfb82e5814c5dfc2d
Wenxuan Wu <wwx970624771@gmail.com>
2024-04-04 14:54:09 -0700

    feat(page):finished navbar and error page UI (#6)
    * feat(page):finished navbar and error page UI
    
    * fix(naming): use camelcase
    
    * fix(style): button style in error page

--- src/pages/Error/NotFound.tsx
+++ src/pages/Error/NotFound.tsx
@@ -1,7 +1,41 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const NotFound = () => {
-  return <div>NotFound</div>;
+const NotFound: React.FC = () => {
+  const [identity, setIdentity] = React.useState<string>('HR');
+
+  const navigate = useNavigate();
+
+  console.log(navigate);
+
+  const goBackHandler = () => {
+    if (identity === 'HR') {
+      navigate('/profiles_hr');
+    } else {
+      navigate('/onboarding');
+    }
+  };
+
+  const message =
+    'The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.';
+  return (
+    <div className="flex flex-grow bg-slate-200">
+      <div className="flex flex-col m-auto gap-8">
+        <div className="text-4xl text-center text-gsray-800">
+          {' '}
+          404 - Not Found
+        </div>
+
+        <div className="text-center text-gray-800">{message}</div>
+        <div className="text-center">
+          <Button variant="default" size="lg" onClick={goBackHandler}>
+            Home
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
