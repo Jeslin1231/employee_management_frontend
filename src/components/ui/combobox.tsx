fffccb76af437ebeba4bbdf52c46e831e17f9f56
Machearn <machearn.n@gmail.com>
2024-04-04 19:57:34 -0400

    feat(page): finished onboarding page layout (#9)

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,79 @@
+import { Check, ChevronsUpDown } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+interface ComboboxProps {
+  open: boolean;
+  value: string;
+  buttonText: string;
+  options: { label: string; value: string }[];
+  placeholder?: string;
+  onOpenChange: (open: boolean) => void;
+  onSelect: (value: string) => void;
+}
+
+export const Combobox = (props: ComboboxProps) => {
+  return (
+    <Popover open={props.open} onOpenChange={props.onOpenChange}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={props.open}
+          className={cn(
+            'justify-between font-normal',
+            !props.value && 'text-muted-foreground',
+          )}
+        >
+          {props.value
+            ? props.options.find(option => option.value === props.value)?.label
+            : props.buttonText}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          {props.placeholder && (
+            <CommandInput placeholder={props.placeholder} />
+          )}
+          <CommandList>
+            <CommandEmpty>No State found.</CommandEmpty>
+            <CommandGroup>
+              {props.options.map(option => (
+                <CommandItem
+                  key={option.value}
+                  value={option.value}
+                  onSelect={props.onSelect}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      props.value === option.value
+                        ? 'opacity-100'
+                        : 'opacity-0',
+                    )}
+                  />
+                  {option.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+};
--- src/pages/Onborading/index.tsx
+++ src/pages/Onborading/index.tsx
@@ -1,7 +1,279 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Combobox } from '@/components/ui/combobox';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { useState } from 'react';
+import { DatePicker } from '@/components/ui/datepicker';
+import { Separator } from '@/components/ui/separator';
+import { genders, states, citizenOp, identities, visaTypes } from './options';
 
 const Onboarding = () => {
-  return <div>Onboarding</div>;
+  const [stateOpen, setStateOpen] = useState(false);
+  const [genderOpen, setGenderOpen] = useState(false);
+  const [citizenOpen, setCitizenOpen] = useState(false);
+  const [identityOpen, setIdentityOpen] = useState(false);
+  const [visaOpen, setVisaOpen] = useState(false);
+  const [stateValue, setStateValue] = useState('');
+  const [genderValue, setGenderValue] = useState('');
+  const [citizen, setCitizen] = useState('');
+  const [identity, setIdentity] = useState('');
+  const [visa, setVisa] = useState('');
+
+  const [date, setDate] = useState<Date>();
+  const [startDate, setStartDate] = useState<Date>();
+  const [endDate, setEndDate] = useState<Date>();
+
+  return (
+    <div className="flex flex-grow bg-slate-200">
+      <div className="flex flex-col w-4/5 mx-auto bg-white">
+        <header className="text-3xl font-semibold my-5 mx-28">
+          Onboarding Application
+        </header>
+        <Separator className="my-5 w-4/5 self-center" />
+        <div className="flex flex-col my-5 mx-44">
+          <header className="text-2xl font-semibold my-5">
+            Personal Information
+          </header>
+          <div className="flex my-5 items-center justify-between w-full">
+            <div className="flex flex-col w-1/12 items-center">
+              <Input type="file" id="avatar" className="hidden" />
+              <Label htmlFor="avatar" className="cursor-pointer">
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="avatar"
+                  />
+                  <AvatarFallback>Profile</AvatarFallback>
+                </Avatar>
+              </Label>
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">First Name</Label>
+              <Input type="text" placeholder="Enter your first name" />
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Last Name</Label>
+              <Input type="text" placeholder="Enter your last name" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/2">
+              <Label className="my-1 text-md font-semibold">Street</Label>
+              <Input type="text" placeholder="Street Line" />
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">APT</Label>
+              <Input type="text" placeholder="APT#" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">City</Label>
+              <Input type="text" placeholder="City" />
+            </div>
+            <div className="flex flex-col w-1/5">
+              <Label className="my-1 text-md font-semibold">State</Label>
+              <Combobox
+                open={stateOpen}
+                value={stateValue}
+                buttonText="State"
+                options={states}
+                placeholder="Select a State..."
+                onOpenChange={setStateOpen}
+                onSelect={currentValue => {
+                  setStateValue(
+                    currentValue === stateValue ? '' : currentValue,
+                  );
+                  setStateOpen(false);
+                }}
+              />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">Zip Code</Label>
+              <Input type="text" placeholder="Zip Code" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/2">
+              <Label className="my-1 text-md font-semibold">Email</Label>
+              <Input type="text" placeholder="Enter your email address" />
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Cell Phone</Label>
+              <Input type="text" placeholder="Enter your phone number" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">SSN</Label>
+              <Input type="text" placeholder="Enter your ssn" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">
+                Date of Birth
+              </Label>
+              <DatePicker value={date} onSelect={setDate} />
+            </div>
+            <div className="flex flex-col w-1/5">
+              <Label className="my-1 text-md font-semibold">Gender</Label>
+              <Combobox
+                open={genderOpen}
+                value={genderValue}
+                buttonText="Gender"
+                options={genders}
+                onOpenChange={setGenderOpen}
+                onSelect={currentValue => {
+                  setGenderValue(
+                    currentValue === genderValue ? '' : currentValue,
+                  );
+                  setGenderOpen(false);
+                }}
+              />
+            </div>
+          </div>
+          <div className="flex my-5 justify-start w-full">
+            <div className="flex flex-col w-2/5">
+              <Combobox
+                open={citizenOpen}
+                value={citizen}
+                buttonText="Permanent resident or citizen of the U.S.?"
+                options={citizenOp}
+                onOpenChange={setCitizenOpen}
+                onSelect={currentValue => {
+                  setCitizen(currentValue === citizen ? '' : currentValue);
+                  setCitizenOpen(false);
+                }}
+              />
+            </div>
+            {citizen === 'yes' && (
+              <div className="ml-10 flex flex-col w-1/5">
+                <Combobox
+                  open={identityOpen}
+                  value={identity}
+                  buttonText="Citizen or Green Car"
+                  options={identities}
+                  onOpenChange={setIdentityOpen}
+                  onSelect={currentValue => {
+                    setIdentity(currentValue === identity ? '' : currentValue);
+                    setIdentityOpen(false);
+                  }}
+                />
+              </div>
+            )}
+          </div>
+          <div className="flex my-5 justify-between items-end w-full">
+            <div className="flex flex-col w-2/5">
+              <Combobox
+                open={visaOpen}
+                value={visa}
+                buttonText="What is your work authorization?"
+                options={visaTypes}
+                onOpenChange={setVisaOpen}
+                onSelect={currentValue => {
+                  setVisa(currentValue === visa ? '' : currentValue);
+                  setVisaOpen(false);
+                }}
+              />
+            </div>
+            <div className="flex flex-col w-1/5">
+              <Label className="my-1 text-md font-semibold">Start Date</Label>
+              <DatePicker value={startDate} onSelect={setStartDate} />
+            </div>
+            <div className="flex flex-col w-1/5">
+              <Label className="my-1 text-md font-semibold">End Date</Label>
+              <DatePicker value={endDate} onSelect={setEndDate} />
+            </div>
+          </div>
+          {visa === 'other' && (
+            <div className="flex my-5 justify-between w-full">
+              <div className="flex flex-col w-1/2">
+                <Input type="text" placeholder="Specify visa type" />
+              </div>
+            </div>
+          )}
+          {visa === 'f1' && (
+            <div className="flex my-5 justify-between w-full">
+              <div className="flex flex-col w-1/2">
+                <Label className="my-1 text-md font-semibold">
+                  OPT Receipt
+                </Label>
+                <Input type="file" />
+              </div>
+            </div>
+          )}
+          <Separator className="my-2 self-center" />
+          <header className="text-2xl font-semibold my-5">
+            Reference Information
+          </header>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">First Name</Label>
+              <Input type="text" placeholder="Enter first name" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">Middle Name</Label>
+              <Input type="text" placeholder="Enter middle name" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">Last Name</Label>
+              <Input type="text" placeholder="Enter last name" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Email</Label>
+              <Input type="text" placeholder="Enter email address" />
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Phone</Label>
+              <Input type="text" placeholder="Enter phone number" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">relationship</Label>
+              <Input type="text" placeholder="Indicate relationship" />
+            </div>
+          </div>
+          <Separator className="my-2 self-center" />
+          <header className="text-2xl font-semibold my-5">
+            Emergency Contact
+          </header>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">First Name</Label>
+              <Input type="text" placeholder="Enter first name" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">Middle Name</Label>
+              <Input type="text" placeholder="Enter middle name" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">Last Name</Label>
+              <Input type="text" placeholder="Enter last name" />
+            </div>
+          </div>
+          <div className="flex my-5 justify-between w-full">
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Email</Label>
+              <Input type="text" placeholder="Enter email address" />
+            </div>
+            <div className="flex flex-col w-1/3">
+              <Label className="my-1 text-md font-semibold">Phone</Label>
+              <Input type="text" placeholder="Enter phone number" />
+            </div>
+            <div className="flex flex-col w-1/4">
+              <Label className="my-1 text-md font-semibold">relationship</Label>
+              <Input type="text" placeholder="Indicate relationship" />
+            </div>
+          </div>
+          <Button variant="link" className="text-blue-600 self-start p-0">
+            +add new
+          </Button>
+        </div>
+        <Button className="w-1/5 mx-auto mb-5">Submit</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Onboarding;
