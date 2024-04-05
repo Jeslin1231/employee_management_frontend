5402fe7adb7f099f4194a9667af3109f6e8a0f07
Wenxuan Wu <wwx970624771@gmail.com>
2024-04-04 18:23:28 -0700

    feat(page):initialize HR visa page

--- src/pages/VisaHr/inProgressVisa.tsx
+++ src/pages/VisaHr/inProgressVisa.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const inProgressVisa = () => {
+  const initialSteps = [
+    'Sent registration token',
+    'submit onboardiing application',
+  ];
+  const visaSteps = [
+    { 'OPT receipt': ['pending', 'rejected', 'approved'] },
+    { 'OPT EAD': ['pending', 'rejected', 'approved'] },
+    { 'I-983': ['pending', 'rejected', 'approved'] },
+    { 'I-20': ['pending', 'rejected', 'approved'] },
+  ];
+
   return <div>inProgressVisa</div>;
 };
 
