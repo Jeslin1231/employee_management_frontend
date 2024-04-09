9ca298e529fb4966516716b2f006339b8a5c796b
Machearn <machearn.n@gmail.com>
2024-04-09 17:32:36 -0400

    feat(auth): implemented custom link adding token to req header (#22)

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,6 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({ uri: 'http://localhost:4000/graphql' });
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      'x-auth-token': operation.variables.token || '',
+    },
+  }));
+
+  return forward(operation);
+});
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
